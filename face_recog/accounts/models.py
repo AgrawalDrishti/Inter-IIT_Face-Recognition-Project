6f34d5ebfb88c65e6712103479bb24b091978217
drishti agrawal <drishti29agrawal@gmail.com>
2022-12-21 01:26:31 +0530

    updated website

--- face_recog/accounts/models.py
+++ face_recog/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Student(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE )
     name = models.CharField(max_length=120, null=True, blank=False)
-    phone = models.CharField(max_length=120, null=True, blank=False)
+    phone = models.CharField(max_length=10, null=True, blank=False)
     email = models.CharField(max_length=120, null=True, blank=False)
     profile_pic = models.ImageField(default="profile_photo.png", null=True, blank=True)
 
